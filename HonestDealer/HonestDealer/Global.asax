811e660b24dce7a1c35174230d2e9d6fbf728579
tcphw5 <tcphw5@mst.edu>
2015-11-12 21:37:23 -0600

    Initial project commit. Azure server established. Project framework in place.

---
+++ HonestDealer/HonestDealer/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HonestDealer.MvcApplication" Language="C#" %>
---
+++ HonestDealer/HonestDealer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HonestDealer.Startup))]
+namespace HonestDealer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
